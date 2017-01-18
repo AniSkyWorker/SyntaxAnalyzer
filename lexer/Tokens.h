2919f9b929a7a66451f56f8e2570dbda1c903344
AniSkyWorker <holyrecycler@mail.ru>
2017-01-18 05:28:12 +0300

    почистил директорию, добавил забытые файлы, привел проекты в порядок

---
+++ lexer/Tokens.h
@@ -0,0 +1,29 @@
+#pragma once
+
+const std::string START_BLOCK = "{";
+const std::string END_BLOCK = "}";
+const std::string OPEN_BRACKET = "(";
+const std::string CLOSE_BRACKET = ")";
+const std::string INDEX_OPEN = "[";
+const std::string INDEX_CLOSE = "]";
+const std::string IF_STATEMENT = "if";
+const std::string WHILE_STATEMENT = "while";
+const std::string ELIF_STATEMENT = "elif";
+const std::string ELSE_STATMENT = "else";
+const std::string LINE_END = ";";
+const std::string PRINT = "print";
+const std::string READ = "read";
+const std::string ID = "id";
+const std::string ASSIGMENT = "=";
+const std::string CONST = "const";
+
+namespace tokens
+{
+
+	const std::string INT = "int";
+	const std::string FLOAT = "float";
+	const std::string CHAR = "char";
+	const std::string STRING = "string";
+	const std::string BOOL = "bool";
+
+}
